4c32554fd47efd77b9850a9637c6728826d51215
Lucas Paiva <lucas.gomes.paiva@usp.br>
2018-11-05 14:57:27 -0200

    Add movement to checker pieces
    Add movement possibility to white pieces.
    Add constraint to allow movement to empty spaces only

--- scripts/checkers.js
+++ scripts/checkers.js
@@ -1,5 +1,8 @@
 const rows = 8;
 const columns = 8;
+let selectedRow = 0;
+let selectedColumn = 0;
+let pieceSelected = 0; // 0 when no piece has been selected. selected piece's type identifier when a piece has been selected
 let board = document.getElementById('board');
 
 let tile = new Array(rows);
@@ -12,12 +15,36 @@ for (let i = 0; i < rows; i++) {
  
 makeBoard(nameUm, nameDois);
 
+function removePieceFromBoard(row, column){
+  tile[row][column].piece = 0;
+  tile[row][column].src = "../pictures/empty_black_square.png";
+}
+
+function placePieceOnBoard(row, column, piece){
+  tile[row][column].piece = piece;   //piece type identifier
+  switch(piece){
+    case 1: 
+      tile[row][column].src = "../pictures/black_common_black_square.png";
+      break;
+    case 2: 
+      tile[row][column].src = "../pictures/black_special_black_square.png";
+      break;
+    case -1: 
+      console.log("yay");
+      tile[row][column].src = "../pictures/white_common_black_square.png";
+      break;
+    case -2: 
+      tile[row][column].src = "../pictures/white_special_black_square.png";
+      break;
+  }
+}
+
 function makeBoard(nameOne, nameTwo) {
-/* Preparar o tabuleiro inicial do jogo.
- * Nas 3 primeiras linhas, popular quadrados verdes (pretos) com pecas pretas
- * Nas 3 ultimas linhas, popular quadrados verdes (pretos) com pecas brancas
- * Nas outras linhas, popular com quadrados vazios
- * Adicionar event listener apenas em quadrados verdes (pretos)
+/* Initialize the game board
+ * On the first 3 rows, populate green squares with black pieces
+ * On the last 3 rows, populate green squares with white pieces
+ * on other rows, populate with empty squares
+ * Add event listener only on green squares
  */
   let nameOneHTML = document.createElement("h1");
   nameOneHTML.innerHTML = nameOne;
@@ -32,24 +59,21 @@ function makeBoard(nameOne, nameTwo) {
     for (column = 0; column < columns; column++) {
       tile[row][column] = document.createElement("img");
       if((row + column)%2 == 0){
-        /* 3 primeiras linhas */
+        /* 3 first rows */
         if(row < 3){
-          tile[row][column].piece = 1;   //black common
-          tile[row][column].src = "../pictures/black_common_black_square.png";
+          placePieceOnBoard(row, column, 1);
         }
         else{
-          /* 3 ultimas linhas */
+          /* 3 last rows */
           if(8 - row < 4){
-            tile[row][column].piece = -1;  //white common
-            tile[row][column].src = "../pictures/white_common_black_square.png";
+            placePieceOnBoard(row, column, -1)
           }
           else {
-            tile[row][column].piece = 0;
-            tile[row][column].src = "../pictures/empty_black_square.png";
+            removePieceFromBoard(row, column);
           }
         }
-        tile[row][column].row = row + 1;
-        tile[row][column].column = column + 1;
+        tile[row][column].row = row;
+        tile[row][column].column = column;
         tile[row][column].addEventListener("mousedown", click);
       }
       else{
@@ -65,18 +89,32 @@ function makeBoard(nameOne, nameTwo) {
   nameTwoHTML.innerHTML = nameTwo;
   nameTwoHTML.style = "position:absolute; text-align: center; margin-left: auto;";
   nameTwoHTML.style.top = (row + 1) * 64;
-  console.log(row);
   board.appendChild(nameTwoHTML);
 }
  
 function click(event) {
   let source = event.target;
-  console.log("Voce clicou em: coluna " + source.column + " linha: " + source.row);
-  if(source.piece < 0) {
-    console.log("Peca branca!");
+  
+  if(pieceSelected != 0){
+    /*Where to move the selected piece?*/
+    if(source.piece == 0){  
+      removePieceFromBoard(selectedRow, selectedColumn);
+      placePieceOnBoard(source.row, source.column, pieceSelected);
+      pieceSelected = 0;
+    }
+    else{
+      board.message = "Please select an empty space to move your piece"
+    }
   }
-  if(source.piece > 0) {
-    console.log("Peca preta!");
+  else{
+    if(source.piece < 0){
+      pieceSelected = source.piece;
+      selectedRow = source.row;
+      selectedColumn = source.column;
+    }
+    else{
+      board.message = "Please select a valid space"
+    }
   }
 }
  
